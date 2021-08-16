d97f3dcfcb03e7cee85380f8ad42fe623d28f6e2
Jo3y_the_Creator <ejusticecudjoe@gmail.com>
2021-08-16 18:27:38 +0100

    Final commit

---
+++ collapse.js
@@ -0,0 +1,199 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.background = "#000000";
+  } else {
+    document.getElementById("navbar").style.background = "none";
+  }
+}
+
+function photoshopHover() {
+  var photoshop = document.getElementById("photoshopProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "block";
+  }
+}
+
+function photoshopOut() {
+  var photoshop = document.getElementById("photoshopProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "none";
+  }
+}
+
+function afterEffectsHover() {
+  var photoshop = document.getElementById("afterEffectsProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "block";
+  }
+}
+
+function afterEffectsOut() {
+  var photoshop = document.getElementById("afterEffectsProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "none";
+  }
+}
+
+function illustratorHover() {
+  var photoshop = document.getElementById("illustratorProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "block";
+  }
+}
+
+function illustratorOut() {
+  var photoshop = document.getElementById("illustratorProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "none";
+  }
+}
+
+function indesignHover() {
+  var photoshop = document.getElementById("indesignProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "block";
+  }
+}
+
+function indesignOut() {
+  var photoshop = document.getElementById("indesignProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "none";
+  }
+}
+
+function premiereProHover() {
+  var photoshop = document.getElementById("premiereProProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "block";
+  }
+}
+function premiereProOut() {
+  var photoshop = document.getElementById("premiereProProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "none";
+  }
+}
+function cssHover() {
+  var photoshop = document.getElementById("cssProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "block";
+  }
+}
+
+function cssOut() {
+  var photoshop = document.getElementById("cssProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "none";
+  }
+}
+
+function htmlHover() {
+  var photoshop = document.getElementById("htmlProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "block";
+  }
+}
+
+function htmlOut() {
+  var photoshop = document.getElementById("htmlProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "none";
+  }
+}
+
+function javascriptHover() {
+  var photoshop = document.getElementById("javascriptProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "block";
+  }
+}
+
+function javascriptOut() {
+  var photoshop = document.getElementById("javascriptProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "none";
+  }
+}
+
+function reactHover() {
+  var photoshop = document.getElementById("reactProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "block";
+  }
+}
+
+function reactOut() {
+  var photoshop = document.getElementById("reactProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "none";
+  }
+}
+
+function responsiveHover() {
+  var photoshop = document.getElementById("responsiveProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "block";
+  }
+}
+function responsiveOut() {
+  var photoshop = document.getElementById("responsiveProgress");
+  if (photoshop === "block") {
+    photoshop.style.display = "none";
+  } else {
+    photoshop.style.display = "none";
+  }
+}
+
+
